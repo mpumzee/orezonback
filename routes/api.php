6516f8b4b259a2aeb3b478decdebf3960cc281ec
Mgunara0305 <vuyelwamguni@gmail.com>
2024-12-05 16:18:53 +0200

    modified search api url

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function() {
 
 Route::apiResource('equipment', EquipmentController::class);
+Route::get('equipment/search/{name}',[EquipmentController::class,'search']);
 
 });
 
@@ -15,4 +16,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/search/{name}',[EquipmentController::class,'search']);
+
