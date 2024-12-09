5e3bc87e8649de16dce414c22ca306f372c7a317
Mgunara0305 <vuyelwamguni@gmail.com>
2024-12-09 18:57:32 +0200

    added the login and register function

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,26 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\EquipmentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function() {
+    // public routes
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/register', [AuthController::class, 'register']);
 
+// protected routes
+Route::middleware(['auth:sanctum'])->group(function () {
 Route::apiResource('equipment', EquipmentController::class);
 Route::get('equipment/search/{name}',[EquipmentController::class,'search']);
-
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::get('/profile', [AuthController::class, 'profile']);
+   
+}); 
 });
+
+
 
 
 Route::get('/user', function (Request $request) {
